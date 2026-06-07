32b3a6423797f793bb23f546ec522292a20e9cf1
Zai619 <tracycarriemomo@gmail.com>
2026-06-07 17:02:17 +0800

    加入專案檔案。

---
+++ WordCards/frmWordCards.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WordCards
+{
+    partial class frmWordCards
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmWordCards));
+            this.sssWord = new System.Windows.Forms.StatusStrip();
+            this.tsslMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lstWordList = new System.Windows.Forms.ListBox();
+            this.palMain = new System.Windows.Forms.Panel();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.btnAutoPlay = new System.Windows.Forms.Button();
+            this.txtHelp = new System.Windows.Forms.TextBox();
+            this.txtPhonogram = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.timPlayer = new System.Windows.Forms.Timer(this.components);
+            this.sssWord.SuspendLayout();
+            this.palMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sssWord
+            // 
+            this.sssWord.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.sssWord.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslMessage});
+            this.sssWord.Location = new System.Drawing.Point(0, 381);
+            this.sssWord.Name = "sssWord";
+            this.sssWord.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.sssWord.Size = new System.Drawing.Size(831, 25);
+            this.sssWord.TabIndex = 0;
+            this.sssWord.Text = "statusStrip1";
+            // 
+            // tsslMessage
+            // 
+            this.tsslMessage.Name = "tsslMessage";
+            this.tsslMessage.Size = new System.Drawing.Size(71, 19);
+            this.tsslMessage.Text = "Message";
+            // 
+            // lstWordList
+            // 
+            this.lstWordList.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lstWordList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstWordList.FormattingEnabled = true;
+            this.lstWordList.ItemHeight = 25;
+            this.lstWordList.Location = new System.Drawing.Point(0, 0);
+            this.lstWordList.Margin = new System.Windows.Forms.Padding(4);
+            this.lstWordList.Name = "lstWordList";
+            this.lstWordList.Size = new System.Drawing.Size(308, 381);
+            this.lstWordList.TabIndex = 1;
+            this.lstWordList.Click += new System.EventHandler(this.lstWordList_Click);
+            this.lstWordList.DoubleClick += new System.EventHandler(this.lstWordList_DoubleClick);
+            // 
+            // palMain
+            // 
+            this.palMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.palMain.Controls.Add(this.picLogo);
+            this.palMain.Controls.Add(this.btnAutoPlay);
+            this.palMain.Controls.Add(this.txtHelp);
+            this.palMain.Controls.Add(this.txtPhonogram);
+            this.palMain.Controls.Add(this.textBox2);
+            this.palMain.Controls.Add(this.txtWord);
+            this.palMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.palMain.Location = new System.Drawing.Point(308, 0);
+            this.palMain.Margin = new System.Windows.Forms.Padding(4);
+            this.palMain.Name = "palMain";
+            this.palMain.Size = new System.Drawing.Size(523, 381);
+            this.palMain.TabIndex = 2;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picLogo.Image = global::WordCards.Properties.Resources.WordCards_Logo;
+            this.picLogo.Location = new System.Drawing.Point(305, 27);
+            this.picLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(169, 166);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 5;
+            this.picLogo.TabStop = false;
+            // 
+            // btnAutoPlay
+            // 
+            this.btnAutoPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAutoPlay.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAutoPlay.Location = new System.Drawing.Point(336, 211);
+            this.btnAutoPlay.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAutoPlay.Name = "btnAutoPlay";
+            this.btnAutoPlay.Size = new System.Drawing.Size(108, 64);
+            this.btnAutoPlay.TabIndex = 4;
+            this.btnAutoPlay.Text = "Play";
+            this.btnAutoPlay.UseVisualStyleBackColor = true;
+            this.btnAutoPlay.Click += new System.EventHandler(this.btnAutoPlay_Click);
+            // 
+            // txtHelp
+            // 
+            this.txtHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHelp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.txtHelp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtHelp.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtHelp.ForeColor = System.Drawing.Color.Red;
+            this.txtHelp.Location = new System.Drawing.Point(317, 295);
+            this.txtHelp.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHelp.Multiline = true;
+            this.txtHelp.Name = "txtHelp";
+            this.txtHelp.Size = new System.Drawing.Size(141, 54);
+            this.txtHelp.TabIndex = 3;
+            this.txtHelp.Text = "Enter下一個\r\nSpace重覆";
+            // 
+            // txtPhonogram
+            // 
+            this.txtPhonogram.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPhonogram.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.txtPhonogram.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPhonogram.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtPhonogram.ForeColor = System.Drawing.Color.Green;
+            this.txtPhonogram.Location = new System.Drawing.Point(19, 97);
+            this.txtPhonogram.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPhonogram.Name = "txtPhonogram";
+            this.txtPhonogram.Size = new System.Drawing.Size(123, 32);
+            this.txtPhonogram.TabIndex = 2;
+            this.txtPhonogram.Text = "ˋæbəkəs";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox2.Location = new System.Drawing.Point(19, 149);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(290, 244);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "<-us: calculus 小圓石>";
+            // 
+            // txtWord
+            // 
+            this.txtWord.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.txtWord.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtWord.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtWord.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtWord.Location = new System.Drawing.Point(19, 27);
+            this.txtWord.Margin = new System.Windows.Forms.Padding(4);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(123, 40);
+            this.txtWord.TabIndex = 0;
+            this.txtWord.Text = "abacus";
+            this.txtWord.TextChanged += new System.EventHandler(this.txtWord_TextChanged);
+            // 
+            // timPlayer
+            // 
+            this.timPlayer.Interval = 2000;
+            this.timPlayer.Tick += new System.EventHandler(this.timPlayer_Tick);
+            // 
+            // frmWordCards
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 406);
+            this.Controls.Add(this.palMain);
+            this.Controls.Add(this.lstWordList);
+            this.Controls.Add(this.sssWord);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(849, 453);
+            this.Name = "frmWordCards";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "單字卡";
+            this.Load += new System.EventHandler(this.frmWordCards_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmWordCards_KeyPress);
+            this.sssWord.ResumeLayout(false);
+            this.sssWord.PerformLayout();
+            this.palMain.ResumeLayout(false);
+            this.palMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip sssWord;
+        private System.Windows.Forms.ToolStripStatusLabel tsslMessage;
+        private System.Windows.Forms.ListBox lstWordList;
+        private System.Windows.Forms.Panel palMain;
+        private System.Windows.Forms.TextBox txtPhonogram;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.TextBox txtHelp;
+        private System.Windows.Forms.Button btnAutoPlay;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Timer timPlayer;
+    }
+}
+
